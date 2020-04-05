a8f35edd65ac3288c6f4b502c1bf82f464ce7093
Zixuan Xu <zi.xuan.xu@icloud.com>
2020-04-05 17:41:40 +1000

    linking frontend & backend

---
+++ frontend/src/components/UpvotesSection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const UpvotesSection = ({ name, upvotes, setArticleInfo }) => {
+    const upvoteArticle = async ()=>{
+        const result = await fetch(`/api/articles/${name}/upvote`,{
+            method: "POST",
+        });
+        
+        const body = await result.json();
+        setArticleInfo(body);
+    }
+
+    return (
+        <div className="upvotes-section">
+            <button onClick={()=>{upvoteArticle()}}>Upvote</button>
+            <p>This article has been upvoted for {upvotes} times</p>
+        </div>
+    )
+}
+
+export default UpvotesSection;
--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
+import CommentList from '../components/CommentList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
 
     if (!article) return <NotFoundPage />
 
@@ -13,12 +27,19 @@ const ArticlePage = ({ match }) => {
 
     return (
         <>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <h3>Other Articles:</h3>
-        <ArticlesList articles={otherArticles} />
+            <h1>{article.title}</h1>
+
+            <UpvotesSection name={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
+
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+
+            <CommentList comments={articleInfo.comments} />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
+
+            <h3>Other Articles:</h3>
+            <ArticlesList articles={otherArticles} />
         </>
     );
 }
