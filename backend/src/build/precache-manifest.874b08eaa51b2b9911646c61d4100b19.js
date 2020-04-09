13b2631193ba7b7ef2448731601d19872b80fc84
Zixuan Xu <zi.xuan.xu@icloud.com>
2020-04-09 18:29:16 +1000

    url

---
+++ backend/src/build/precache-manifest.874b08eaa51b2b9911646c61d4100b19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6515fa169cc540df231",
+    "url": "/static/css/main.1b56df56.chunk.css"
+  },
+  {
+    "revision": "c6515fa169cc540df231",
+    "url": "/static/js/main.bbec13c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71b6dd9f4d1e86605b4b",
+    "url": "/static/js/2.e12a084a.chunk.js"
+  },
+  {
+    "revision": "9b5c7807d90dde5944de6abe6896e1b4",
+    "url": "/index.html"
+  }
+];
