a685b17cf70146fff1436f3e90716db41d5b3c98
Zixuan Xu <zi.xuan.xu@icloud.com>
2020-04-09 12:23:21 +1000

    relative path homepage

---
+++ backend/src/build/precache-manifest.ed561ee7363c2afc79f56fc83db07352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c14b0917b9502e0714f7",
+    "url": "./static/css/main.1b56df56.chunk.css"
+  },
+  {
+    "revision": "c14b0917b9502e0714f7",
+    "url": "./static/js/main.4baab423.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "71b6dd9f4d1e86605b4b",
+    "url": "./static/js/2.e12a084a.chunk.js"
+  },
+  {
+    "revision": "02362963ed15419f52b78bad62dbad64",
+    "url": "./index.html"
+  }
+];
