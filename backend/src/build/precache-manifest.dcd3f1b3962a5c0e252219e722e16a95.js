e5afec2a182d95b050183e99d7e874e3ecbd8d7a
Zixuan Xu <zi.xuan.xu@icloud.com>
2020-04-09 23:44:13 +1000

    Revert "url"
    This reverts commit e42ef2dfb492a235675785afb7f82c51d52d8f3a.

---
+++ backend/src/build/precache-manifest.dcd3f1b3962a5c0e252219e722e16a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89fbe5bc2fcbe6bfa68a",
+    "url": "/blog/static/css/main.1b56df56.chunk.css"
+  },
+  {
+    "revision": "89fbe5bc2fcbe6bfa68a",
+    "url": "/blog/static/js/main.e32d168a.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "71b6dd9f4d1e86605b4b",
+    "url": "/blog/static/js/2.e12a084a.chunk.js"
+  },
+  {
+    "revision": "a2cd3fb03ba1f3526c69378e9bf4b8ba",
+    "url": "/blog/index.html"
+  }
+];
